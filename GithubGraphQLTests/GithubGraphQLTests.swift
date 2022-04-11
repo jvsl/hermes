df4fc880fcc4d0775603da4edd2c623c670b16c3
João Victor <ccjoaovictor@gmail.com>
2022-04-11 14:53:05 -0300

    unit tests for viewModel search/next page

--- GithubGraphQLTests/GithubGraphQLTests.swift
+++ GithubGraphQLTests/GithubGraphQLTests.swift
@@ -3,31 +3,116 @@ import XCTest
 
 class GithubGraphQLTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // Private vars are reseted by XCTestCase every test performed
+    private var loader = LoaderStub()
+    private var viewModelSpy = ViewModelSpy()
+    private var sut: GithubGraphQLViewModel {
+        let viewModel = GithubGraphQLViewModel(isConected: false, loader: loader)
+        viewModel.delegate = viewModelSpy
+        
+        return viewModel
     }
+    
+    func testSearch_succesCase_shouldCallFechDataStartAndStopLoading() {
+        let expectation = self.expectation(description: "loading")
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testExample() {
-      let mockedResponse = SearchRepositoriesQuery.Data(search: .init(
-        pageInfo: .init(startCursor: "startCursor", endCursor: nil, hasNextPage: false, hasPreviousPage: false),
-        edges: makeEdges(count: 3)
-      ))
-      let viewModel = ViewModel(client: MockGraphQLClient<SearchRepositoriesQuery>(response: mockedResponse))
-
-      /* add assertions to validate view model state after making requests */
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        sut.search(phrase: "teste", filter: nil) {
+            expectation.fulfill()
+            XCTAssertEqual(self.viewModelSpy.didCallFechData, 1)
+            XCTAssertEqual(self.viewModelSpy.didStartLoading, 1)
+            XCTAssertEqual(self.viewModelSpy.didStopLoading, 1)
         }
+        
+        waitForExpectations(timeout: 1, handler: nil)
     }
 
+    func testSearch_failureCase_shouldCallFechFailStartAndStopLoading() {
+        loader.result = .failure(NSError())
+
+        let expectation = self.expectation(description: "loading")
+        
+        sut.search(phrase: "teste", filter: nil) {
+            expectation.fulfill()
+            XCTAssertEqual(self.viewModelSpy.didFechFaild, 1)
+            XCTAssertEqual(self.viewModelSpy.didStartLoading, 1)
+            XCTAssertEqual(self.viewModelSpy.didStopLoading, 1)
+        }
+
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func testNextPage_successCaseWith3Calls_shouldCallFechData3Times() {
+        let sut = sut
+        let amountOfCallsExpected = 3
+        let expectation = self.expectation(description: "loading")
+
+        sut.search(phrase: "teste", filter: nil) {
+            sut.nextPage {
+                sut.nextPage {
+                    expectation.fulfill()
+                    XCTAssertEqual(self.viewModelSpy.didCallFechData, amountOfCallsExpected)
+                }
+            }
+        }
+
+        waitForExpectations(timeout: 2, handler: nil)
+    }
+}
+
+final class LoaderStub: DataLoading {
+    
+    var result: Result<RepositorySearchResult, Error> =  .success(.init(
+        pageInfo: .init(
+            startCursor: "startCursor",
+            endCursor: "endCursos",
+            hasNextPage: true,
+            hasPreviousPage: false),
+        repos: RepositoryDetails.fixtures(amount: 3)))
+    
+    func load(isConnected: Bool, phrase: String, filter: SearchRepositoriesQuery.Filter?, completion: @escaping FetcherCompletion) {
+        completion(result)
+    }
+}
+
+extension RepositoryDetails {
+    static func fixtures(amount: Int) -> [RepositoryDetails] {
+        
+        let repository = RepositoryDetails(
+            name: "",
+            url: "",
+            owner: Owner.makeUser(login: "", avatarUrl: ""),
+            stargazers: Stargazer(totalCount: 1))
+        
+        return (0...amount).map{ _ in  repository }
+        
+    }
+}
+
+final class ViewModelSpy: GithubGraphQLViewModelDelegate {
+    var didCallFechData = 0
+    var didFechFaild = 0
+    var didOpenURL = 0
+    var didStartLoading = 0
+    var didStopLoading = 0
+    
+    func didFetchData() {
+        didCallFechData += 1
+    }
+    
+    func didFetchFail(action: @escaping () -> Void) {
+        didFechFaild += 1
+    }
+    
+    func openUrlInBrowser(_ url: URL) {
+        didOpenURL += 1
+    }
+    
+    func startLoading() {
+        didStartLoading += 1
+    }
+    
+    func stopLoading() {
+        didStopLoading += 1
+    }
 }
